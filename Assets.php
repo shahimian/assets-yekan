733684a4bfe5cd1e7353c17168807e06fc069184
a.shahimian <shahimian@gmail.com>
2019-06-16 15:57:09 +0430

    initial project

---
+++ Assets.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shahimian
+ * Date: 7/7/2016
+ * Time: 9:26 PM
+ */
+
+namespace app\assets\yekan;
+
+
+use yii\web\AssetBundle;
+
+class Assets extends AssetBundle
+{
+
+    public $sourcePath = "@app/assets/yekan/font";
+
+    public $css = [
+        'font.css',
+    ];
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+use this class to any page following below:
+use app\assets\yekan\Assets as Yekan;
+and loading this one:
+Yekan::register($this);
+Any property of css you want to assign to font-family of Yekan, 'Yekan Numbers Regular'
